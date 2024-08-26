f70a2bf4df2fe23b2ba8a6bbec5893d957326c27
vivek17.gupta <vivek17.Gupta@ril.com>
2024-08-27 03:22:15 +0530

    Third code commit v1.1

--- cal.py
+++ cal.py
@@ -26,16 +26,15 @@ def calculator():
         num2 = float(input("Enter second number: "))
 
         if choice == '1':
-            print(f"{num1} + {num2} = {add(num1, num2)}")
+            print(f"First value {num1} + Second value {num2} = {add(num1, num2)}")
         elif choice == '2':
-            print(f"{num1} - {num2} = {subtract(num1, num2)}")
+            print(f"First value {num1} - Second value {num2} = {subtract(num1, num2)}")
         elif choice == '3':
-            print(f"{num1} * {num2} = {multiply(num1, num2)}")
+            print(f"First value {num1} * Second value {num2} = {multiply(num1, num2)}")
         elif choice == '4':
-            print(f"{num1} / {num2} = {divide(num1, num2)}")
+            print(f"First value {num1} / Second value {num2} = {divide(num1, num2)}")
     else:
         print("Invalid input")
 
 if __name__ == "__main__":
     calculator()
-
